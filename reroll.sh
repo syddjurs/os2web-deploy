b6ad5d9e521ef7aded8af26f84cdfd920fc5cad2
Anders Bryrup <andersbryrup@gmail.com>
2013-08-26 10:36:55 +0200

    Add extra verification that we want to deploy.

--- reroll.sh
+++ reroll.sh
@@ -44,42 +44,50 @@ drush make --no-gitinfofile -y --no-core --contrib-destination=build/$BUILD_DIR 
 if [ -d "build/$BUILD_DIR/modules" ]; then
 	# Drush make completed without errors. If modules doesnt exist, drush make failed.
 
-	# Lets copy our drupal profile files
-	cp $PROFILE_SRC.info build/$BUILD_DIR/$PROFILE_DST.info
-	cp $PROFILE_SRC.profile build/$BUILD_DIR/$PROFILE_DST.profile
-	cp $PROFILE_SRC.install build/$BUILD_DIR/$PROFILE_DST.install
+    echo "Are you really sure you want to deploy the newly made build? (y/n)"
+    CONTINUE=
+    read CONTINUE
+    if [ "$CONTINUE" = "y" ]; then
+		# Lets copy our drupal profile files
+		cp $PROFILE_SRC.info build/$BUILD_DIR/$PROFILE_DST.info
+		cp $PROFILE_SRC.profile build/$BUILD_DIR/$PROFILE_DST.profile
+		cp $PROFILE_SRC.install build/$BUILD_DIR/$PROFILE_DST.install
 
-	# Move old build to previous
-	unlink build/$BUILD_DIR_PREV
-	mv build/$BUILD_DIR_LATEST build/$BUILD_DIR_PREV
-	# Make new build the latest
-	ln -sf $BUILD_DIR build/$BUILD_DIR_LATEST
+		# Move old build to previous
+		unlink build/$BUILD_DIR_PREV
+		mv build/$BUILD_DIR_LATEST build/$BUILD_DIR_PREV
+		# Make new build the latest
+		ln -sf $BUILD_DIR build/$BUILD_DIR_LATEST
 
-	# Take a copy of the current database,
-	# and put it in the previous build. Code and database keeps together.
-	echo "Backing up the database..."
-		# TODO: skip basic tables like cache --structure-tables-key=#{tables}
-		# Will make the dump smaller.
-	drush sql-dump --root=$DRUPAL_ROOT --uri=$URI --gzip > build/$BUILD_DIR_PREV/sql-dump.$DATE.sql.gz
+		# Take a copy of the current database,
+		# and put it in the previous build. Code and database keeps together.
+		echo "Backing up the database..."
+			# TODO: skip basic tables like cache --structure-tables-key=#{tables}
+			# Will make the dump smaller.
+		drush sql-dump --root=$DRUPAL_ROOT --uri=$URI --gzip > build/$BUILD_DIR_PREV/sql-dump.$DATE.sql.gz
 
-	echo "Updating database... Site will go in maintenance mode!"
-	drush --root=$DRUPAL_ROOT --uri=$URI vset maintenance_mode 1
-	drush --root=$DRUPAL_ROOT --uri=$URI updb
+		echo "Updating database... Site will go in maintenance mode!"
+		drush --root=$DRUPAL_ROOT --uri=$URI vset maintenance_mode 1
+		drush --root=$DRUPAL_ROOT --uri=$URI updb
 
-	# # Any additionally drush commands?
+		# # Any additionally drush commands?
 
-	# Finnally clear the cache
-	echo "Clearing cache..."
-	drush --root=$DRUPAL_ROOT --uri=$URI cc registry
-	drush --root=$DRUPAL_ROOT --uri=$URI cc all
+		# Finnally clear the cache
+		echo "Clearing cache..."
+		drush --root=$DRUPAL_ROOT --uri=$URI cc registry
+		drush --root=$DRUPAL_ROOT --uri=$URI cc all
 
-	drush --root=$DRUPAL_ROOT --uri=$URI vset maintenance_mode 0
+		drush --root=$DRUPAL_ROOT --uri=$URI vset maintenance_mode 0
 
-	echo "Deploy Complete. End of maintenance mode!"
+		echo "Deploy Complete. End of maintenance mode!"
 
-	# Cleanup old builds.
-	echo "Deleting old build dirs..."
-	. ./cleanup.sh
+		# Cleanup old builds.
+		echo "Deleting old build dirs..."
+		. ./cleanup.sh
+	else
+		rm -rf build/$BUILD_DIR
+		echo "Deploy terminated!"
+	fi
 else
 	# Build failed, remove build
 	rm -rf build/$BUILD_DIR
